6b6582fd02925c789ba5012928a93a90cbb57fde
beeliott <eliott.bellais@ynov.com>
2023-09-14 15:33:42 +0200

    files reorder

---
+++ src/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "red"
+
+func main() {
+    // Création d'un personnage
+    joueur := Personnage{
+        Nom:        "Joueur1",
+        Classe:     "Guerrier",
+        Niveau:     1,
+        PVMaximum:  100,
+        PVActuels:  100,
+        Inventaire: []string{"Épée", "Armure légère"},
+    }
+
+    // Affichage des détails du personnage
+    joueur.AfficherDetails()
+}
