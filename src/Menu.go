38c1227068c1d73804566ce7440fcc6e985f56fa
beeliott <eliott.bellais@ynov.com>
2023-09-14 15:13:21 +0200

    créa et detail personnae

--- src/Menu.go
+++ src/Menu.go
@@ -1,11 +1,25 @@
 package red
 
+import "fmt"
 
-type personnage interface {
-	name : string
-	class : string
-	level : int
-	pv_max : int
-	pv : int
-	inventory : string
+// Détails personnages
+
+type personnage interface { 
+	name  string
+	class  string
+	level  int
+	pv_max  int
+	pv  int
+	inventory  []string
+}
+
+// Créations personnages
+
+func(p personnage) DetailsPerso{
+	fmt.Printf("Name: %s\n", p.name)
+	fmt.Printf("Class %s\n", p.class)
+	fmt.Printf("Level %s\n", p.level)
+	fmt.Printf("Pv Max %s\n", p.pv_max)
+	fmt.Printf("Pv %s\n", p.pv)
+	fmt.Printf("Inventory %s\n", p.inventory)
 }
